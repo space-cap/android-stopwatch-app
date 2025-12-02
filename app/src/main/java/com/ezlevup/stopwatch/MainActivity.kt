d03fcc1083d2e7886c837ea45596ec57647a4a23
이영현 <hong.git@yahoo.com>
2025-12-02 15:18:06 +0900

    MainScreen 추가

--- app/src/main/java/com/ezlevup/stopwatch/MainActivity.kt
+++ app/src/main/java/com/ezlevup/stopwatch/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import com.ezlevup.stopwatch.ui.theme.SimpleStopWatchTheme
 import java.util.Timer
@@ -30,7 +36,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 
     private var time = 0
     private var timerTask: Timer? = null
@@ -42,8 +48,12 @@ class MainViewModel: ViewModel() {
     val sec: State<Int> = _sec
 
     private val _milli = mutableStateOf(0)
-    val min: State<Int> = _milli
+    val milli: State<Int> = _milli
 
+    private val _lapTimes = mutableStateOf(emptyList<String>())
+    val lapTimes: State<List<String>> = _lapTimes
+
+    private var lap = 1
 
 
     fun start() {
@@ -53,18 +63,58 @@ class MainViewModel: ViewModel() {
             _sec.value = time / 100
             _milli.value = time % 100
         }
-
     }
 
     fun pause() {
-
+        _isRunning.value = false
+        timerTask?.cancel()
     }
 
     fun reset() {
+        timerTask?.cancel()
+        time = 0
+        _sec.value = 0
+        _milli.value = 0
+        _isRunning.value = false
 
+        _lapTimes.value = emptyList()
+        lap = 1
     }
 
     fun recordLapTime() {
+        _lapTimes.value = listOf("$lap LAP : ${sec.value}.${milli.value}") + _lapTimes.value
+        lap++
+    }
+}
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(
+    sec: Int,
+    milli: Int,
+    isRunning: Boolean,
+    lapTimes: List<String>,
+    onReset: () -> Unit,
+    onToggle: () -> Unit,
+    onLapTime: () -> Unit,
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("StopWatch") }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(modifier = Modifier.height(40.dp))
+
+
+        }
     }
 }
